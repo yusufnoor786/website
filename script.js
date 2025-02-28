738e570bbe9dcd3549796618f012a9472f51db56
Zabuka Zuzu <70579549+yusufnoor786@users.noreply.github.com>
2025-02-28 23:06:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-window.addEventListener("scroll", function() {
-    let sections = document.querySelectorAll("section");
-    sections.forEach(sec => {
-        let position = sec.getBoundingClientRect().top;
-        let screenPos = window.innerHeight / 1.2;
-        if (position < screenPos) {
-            sec.style.opacity = "1";
-            sec.style.transform = "translateY(0)";
-        }
-    });
-});
+function toggleMenu() {
+    document.querySelector(".nav-menu").classList.toggle("active");
+}
